ce619e0f7236e93651982e1279c33f12746adac9
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2023-12-09 09:19:32 -0300

    cria spaceship enemy

---
+++ SpaceshipEnemy.cpp
@@ -0,0 +1,51 @@
+#include "SpaceshipEnemy.hpp"
+
+SpaceshipEnemy::SpaceshipEnemy(const ObjetoDeJogo &obj, int life, int velAtaque) : Enemy(obj, life, velAtaque) {}
+
+void SpaceshipEnemy::update() {
+    switch (getState()) {
+        case EnemyState::Idle:
+            moveTo(getPosL(), getPosC());
+            setState(EnemyState::MovingRight);
+            break;
+        case EnemyState::MovingLeft:
+            moveLeft(1);
+            if (getPosC() == 3) {
+                setState(EnemyState::MovingFront);
+            } else if (getPosC() == 45) {
+                setState(EnemyState::MovingFront);
+            } else if (getPosC() == 0) {
+                setState(EnemyState::MovingRight);
+            }
+            break;
+        case EnemyState::MovingRight:
+            moveRight(1);
+            if (getPosC() == 78) {
+                setState(EnemyState::MovingFront);
+            } else if (getPosC() == 144) {
+                setState(EnemyState::MovingFront);
+            } else if (getPosC() == 154) {
+                setState(EnemyState::MovingLeft);
+            }
+            break;
+        case EnemyState::MovingFront:
+            moveDown(2);
+            if (getPosL() >= 40) {
+                setState(EnemyState::MovingBack);
+            }
+            break;
+        case EnemyState::MovingBack:
+            moveUp(1);
+            if (getPosL() == 12 && getPosC() == 3) {
+                setState(EnemyState::MovingLeft);
+            } else if (getPosL() == 12 && getPosC() == 45) {
+                setState(EnemyState::MovingLeft);
+            } else if (getPosL() == 12 && getPosC() == 78) {
+                setState(EnemyState::MovingRight);
+            } else if (getPosL() == 12 && getPosC() == 144) {
+                setState(EnemyState::MovingRight);
+            }
+            break;
+    }
+
+}
