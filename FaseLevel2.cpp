ef274608752e23ef52c652dd222db3875fc56f83
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2023-12-09 09:21:19 -0300

    correções nas fases

--- FaseLevel2.cpp
+++ FaseLevel2.cpp
@@ -1,23 +1,23 @@
-#include "FaseLevel1.hpp"
+#include "FaseLevel2.hpp"
 #include <ctime>
 #include <cstdlib>
 
-void FaseLevel1::init() {
-    spaceship = new Spaceship(ObjetoDeJogo("Spaceship", Sprite("rsc_2/spaceship.img"), 42, 70), 20, 1);
+void FaseLevel2::init() {
+    spaceship = new Spaceship(ObjetoDeJogo("Spaceship", Sprite("rsc_2/spaceship.img"), 42, 70), 100, 1);
     objs.push_back(spaceship);
 
     countEnemy = 0;
     int pos = 0;
-    for (int i = 70; i < 101; i+=10) {
+    for (int i = 10; i < 101; i += 10) {
         countEnemy += 1;
-        alien[pos] = new Alien (ObjetoDeJogo("Alien", Sprite("rsc_2/alien.img"), 0, i));
+        alien[pos] = new Alien(ObjetoDeJogo("Alien", Sprite("rsc_2/alien.img"), 0, i));
         objs.push_back(alien[pos++]);
     }
 
     pos = 0;
-    for (int i = 80; i < 101; i+=10) {
+    for (int i = 60; i < 131; i += 10) {
         countEnemy += 1;
-        jellyfish[pos] = new Jellyfish (ObjetoDeJogo("Jellyfish", Sprite("rsc_2/jellyfish.img"), 8, i));
+        jellyfish[pos] = new Jellyfish(ObjetoDeJogo("Jellyfish", Sprite("rsc_2/jellyfish.img"), 8, i));
         objs.push_back(jellyfish[pos++]);
     }
 
@@ -25,17 +25,20 @@ void FaseLevel1::init() {
     SpriteBase *tmp = const_cast<SpriteBase *> (objs.back()->getSprite());
     life = dynamic_cast<TextSprite *> (tmp);
 
-    objs.push_back(new ObjetoDeJogo("Shots", TextSprite("0/20"), 48, 140));
+    objs.push_back(new ObjetoDeJogo("Shots", TextSprite("0/100"), 48, 140));
     SpriteBase *tmp2 = const_cast<SpriteBase *> (objs.back()->getSprite());
     shots = dynamic_cast<TextSprite *> (tmp2);
 
     controller = new ShotController(ObjetoDeJogo("ShotController", Sprite("rsc_2/shot.img"), 0, 0));
     objs.push_back(controller);
 
+    itemController = new ItemController(ObjetoDeJogo("ItemController", Sprite("rsc_2/shot.img"), 0, 0));
+    objs.push_back(itemController);
+
     srand(time(NULL));
 }
 
-unsigned FaseLevel1::run(SpriteBuffer &screen) {
+unsigned FaseLevel2::run(SpriteBuffer &screen) {
     std::string ent;
 
     //padrão
@@ -45,6 +48,7 @@ unsigned FaseLevel1::run(SpriteBuffer &screen) {
 
     int cont = 0;
     int colisao;
+    int cont2 = 0;
     while (true) {
         //lendo entrada
         system("stty raw");
@@ -58,26 +62,48 @@ unsigned FaseLevel1::run(SpriteBuffer &screen) {
             spaceship->moveLeft(3);
         } else if (ent == "d" && spaceship->getPosC() < 154) {
             spaceship->moveRight(3);
-        } else if (ent == "x" && spaceship->getQuantShots() < spaceship->getTotalShots() ) {
+        } else if (ent == "x" && spaceship->getQuantShots() < spaceship->getTotalShots()) {
             controller->createShot(spaceship->getPosL() - 2, spaceship->getPosC() + 5, spaceship->getVelShot(), 1);
             spaceship->incrementQuantShots();
             shots->setText(std::to_string(spaceship->getQuantShots()) + "/" + std::to_string(spaceship->getTotalShots()));
         }
 
         cont++;
-        if (cont == 20) {
+        if (cont >= 20) {
             int i = rand() % 4;
             if (alien[i]->isActive()) {
                 controller->createShot(alien[i]->getPosL() + 5, alien[i]->getPosC() + 2, 1, -1);
+                cont = 0;
             }
-            cont = 0;
         }
 
         colisao = controller->verificaColisao(objs);
         if (colisao == 1) {
             countEnemy--;
         } else if (colisao == 0) {
-            life->setText(std::string(spaceship->getLife()/10,'#'));
+            life->setText(std::string(spaceship->getLife() / 10, '#'));
+        }
+
+        cont2++;
+        if (cont2 >= 60) {
+            int i = rand() % 3;
+            int j = rand() % 3;
+            if (jellyfish[i]->isActive()) {
+                itemController->createItem(jellyfish[i]->getPosL() + 5, jellyfish[i]->getPosC() + 2, 1, j);
+                cont2 = 0;
+            }
+        }
+
+        colisao = itemController->verificaColisao(*spaceship);
+        if (colisao == 0) {
+            spaceship->recebeItemLife();
+            life->setText(std::string(spaceship->getLife() / 10, '#'));
+        } else if (colisao == 1) {
+            spaceship->recebeItemNocivo();
+            life->setText(std::string(spaceship->getLife() / 10, '#'));
+        } else if (colisao == 2) {
+            spaceship->recebeItemShot();
+            shots->setText(std::to_string(spaceship->getQuantShots()) + "/" + std::to_string(spaceship->getTotalShots()));
         }
 
         update();
@@ -87,10 +113,12 @@ unsigned FaseLevel1::run(SpriteBuffer &screen) {
 
         if (countEnemy == 0) {
             return Fase::LEVEL_COMPLETE;
-        } else if ((spaceship->getQuantShots() == spaceship->getTotalShots() && countEnemy > 0) || (!spaceship->isAlive())) {
+        } else if ((spaceship->getQuantShots() == spaceship->getTotalShots() && countEnemy > 0) ||
+                   (!spaceship->isAlive())) {
             return Fase::GAME_OVER;
         }
 
         show(screen);
     }
+
 }
---
+++ FaseLevel2.hpp
@@ -0,0 +1,42 @@
+#ifndef FASELEVEL2_HPP
+#define FASELEVEL2_HPP
+
+#include "ASCII_Engine/Fase.hpp"
+#include "Spaceship.hpp"
+#include "Enemy.hpp"
+#include "ShotController.hpp"
+#include "Jellyfish.hpp"
+#include "Alien.hpp"
+#include "SpaceshipEnemy.hpp"
+#include "Shot.hpp"
+#include "Alien.hpp"
+#include "Jellyfish.hpp"
+#include "ItemController.hpp"
+
+class FaseLevel2 : public Fase
+{
+public:
+    FaseLevel2(std::string name, const Sprite &bkg) : Fase(name,bkg) {}
+    FaseLevel2(std::string name, const SpriteAnimado &bkg) : Fase(name,bkg) {}
+    virtual ~FaseLevel2() {}
+
+    virtual void init();
+    virtual unsigned run(SpriteBuffer &screen);
+
+    int getContEnemy() const { return countEnemy; }
+    void setContEnemy(int num) { countEnemy = num; }
+private:
+    Spaceship *spaceship;
+    Shot *shot[10];
+    Alien *alien[10];
+    Enemy *jellyfish[8];
+    TextSprite *life, *shots;
+    ShotController *controller;
+    ItemController *itemController;
+    int countEnemy;
+//
+//	enum {RIDING, PRACING, END};
+
+};
+
+#endif // FASELEVEL2_HPP
