b956b5950cab777ae34f87997d2a560f28ce1a15
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2023-12-11 12:14:02 -0300

    adiciona fase de apresentação do jogo

---
+++ Apresentacao.cpp
@@ -0,0 +1,42 @@
+#include "Apresentacao.hpp"
+
+void Apresentacao::init()
+{
+    carta = new ObjetoDeJogo("Carta", Sprite("rsc_2/carta.img"), 13, 50);
+    objs.push_back(carta);
+
+    objs.push_back(new ObjetoDeJogo("Carregamento", TextSprite("#"), 49, 50));
+    SpriteBase *tmp = const_cast<SpriteBase *> (objs.back()->getSprite());
+    carregamento = dynamic_cast<TextSprite *> (tmp);
+}
+
+unsigned Apresentacao::run(SpriteBuffer &screen) {
+    std::string ent;
+
+    //padrão
+    draw(screen);
+    system("clear");
+    show(screen);
+
+    int contagem = 2;
+    for (int i = 0; i < 50; i++) {
+        //lendo entrada
+        getline(std::cin, ent);
+        if (ent == "q")
+            return Fase::END_GAME;
+
+        carregamento->setText(std::string(contagem, '#'));
+        contagem++;
+
+
+        update();
+        screen.clear();
+        draw(screen);
+        system("clear");
+
+        if (contagem == 100)
+            return Fase::LEVEL_COMPLETE;
+
+        show(screen);
+    }
+}
