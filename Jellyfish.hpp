c89652ca09f11facfe9bda3cf5afdf1fb05c4b33
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2023-12-11 23:35:42 -0300

    importa Enemy

--- Jellyfish.hpp
+++ Jellyfish.hpp
@@ -2,15 +2,14 @@
 #define JELLYFISH_HPP
 
 #include "ASCII_Engine/ObjetoDeJogo.hpp"
+#include "Enemy.hpp"
 
 class Jellyfish : public Enemy
 {
 public:
-	Jellyfish(const ObjetoDeJogo &obj, int life=30, int velAtaque=3) : Enemy(obj, life, velAtaque) {}
+	Jellyfish(const ObjetoDeJogo &obj) : Enemy(obj) {}
 	
-	virtual ~Jellyfish(){}
-
-
+	virtual ~Jellyfish() {}
 };
 
-#endif // ENEMY_HPP
+#endif
